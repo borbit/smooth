01a20bcc3af35318134030cdff09a6e10870ae82
Serge Borbit <serge.borbit@gmail.com>
2015-10-17 14:43:38 +0300

    wip

--- public/blocks/form/form_payment.js
+++ public/blocks/form/form_payment.js
@@ -2,29 +2,53 @@ let $ = require('jquery')
 let $document = $(document)
 
 let $form = $('.form_payment')
+let $inputPhone = $form.find('[name="customer[phone_number]"]')
+let $inputName = $form.find('[name="customer[name]"]')
 let $inputCard = $form.find('[name="card[number]"]')
 let $inputExp = $form.find('[name="card[exp]"]')
 let $inputCvc = $form.find('[name="card[cvc]"]')
 let $spinner = $form.find('.form__spinner')
 let $submit = $form.find('button')
 
+let isPhoneValid = false
+let isNameValid = false
+let isCardValid = false
+let isExpValid = false
+let isCvcValid = false
+
+$inputPhone.inputmask('+9 999 9999999')
 $inputCard.payment('formatCardNumber')
 $inputExp.payment('formatCardExpiry')
 $inputCvc.payment('formatCardCVC')
 
+$inputPhone.on('keyup', validate)
+$inputName.on('keyup', validate)
 $inputCard.on('keyup', validate)
 $inputExp.on('keyup', validate)
 $inputCvc.on('keyup', validate)
+
+$inputPhone.on('blur', () => {$inputPhone.toggleInputError(isPhoneValid)})
+$inputName.on('blur', () => {$inputName.toggleInputError(isNameValid)})
+$inputCard.on('blur', () => {$inputCard.toggleInputError(isCardValid)})
+$inputExp.on('blur', () => {$inputExp.toggleInputError(isExpValid)})
+$inputCvc.on('blur', () => {$inputCvc.toggleInputError(isCvcValid)})
 
 $document.on('screen:payment', () => {
   $form.find('.form__input')[0].focus()
 })
 
+$.fn.toggleInputError = function(valid) {
+  return this.toggleClass('form__input_error', !valid)
+}
+
 function validate() {
   let cardType = $.payment.cardType($('.cc-number').val());
-  let isCardValid = $.payment.validateCardNumber($inputCard.val())
-  let isExpValid = $.payment.validateCardExpiry($inputExp.payment('cardExpiryVal'))
-  let isCvcValid = $.payment.validateCardCVC($inputCvc.val(), cardType)
+
+  isPhoneValid = !!$inputPhone.val().length
+  isNameValid = !!$inputName.val().length
+  isCardValid = $.payment.validateCardNumber($inputCard.val())
+  isExpValid = $.payment.validateCardExpiry($inputExp.payment('cardExpiryVal'))
+  isCvcValid = $.payment.validateCardCVC($inputCvc.val(), cardType)
 
   if (isCardValid && isExpValid && isCvcValid) {
     $submit.attr('disabled', false)
--- public/js/common.js
+++ public/js/common.js
@@ -2,4 +2,5 @@ require('jquery') //@expose
 require('./libs/jquery.payment')
 require('./libs/jquery.lazyload')
 require('./libs/typed')
+require('jquery.inputmask')
 require('jquery.finger')
