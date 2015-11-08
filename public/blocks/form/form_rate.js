785e588488e90cab0609f8005248932c42750a69
fabulouselephant <themostfabulouselephant@gmail.com>
2015-11-08 10:32:58 +0200

    wip

--- public/blocks/form/form_rate.js
+++ public/blocks/form/form_rate.js
@@ -1,11 +1,11 @@
 let $ = require('jquery')
 
-$('.form__rate-star').on('mousemove', setRate)
+$('.form__rate-stars').on('mousemove', setRate)
 
 function setRate(e) {
   var offset = e.offsetX;
-  var width = $('.form__rate-star').width();
+  var width = $('.form__rate-stars').width();
   var star_width = (width/100)*20;
   var rate = Math.round(offset/star_width);
-  $('.form__rated-star').css('width', rate * 20 + "%");
+  $('.form__rated-stars').css('width', rate * 20 + "%");
 }
